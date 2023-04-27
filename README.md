0045213ec575bc9be7b94a02877079758b27e18f
lpolezhaeva <kroshic555@mail.ru>
2023-04-27 08:58:17 +0200

    README content is added and log_parser is updated.

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# python-otus-logs-analysis
+# Access Log Analyzer
+
+This is a Python script for analyzing access log files in the following format:
+
+```
+%h %t "%r" %>s %b "%{Referer}i" "%{User-Agent}i" %D
+```
+
+The script analyzes the log files and provides the following statistics:
+
+- Total number of requests
+- Number of requests by HTTP methods (GET, POST, PUT, HEAD, OPTIONS, DELETE)
+- Top 3 IP addresses that made requests
+- Top 3 longest requests, including method, URL, IP, duration, and request date and time
+
+The script can analyze a single log file or all log files in a directory.
+
+## Usage
+
+To use the script, run the following command:
+
+```
+python log_parser.py [log_file_or_directory]
+```
+
+Replace `[log_file_or_directory]` with the path to the log file or directory you want to analyze. If you don't specify a log file or directory, the script will analyze the `access.log` file in the current directory.
+
+The script will generate a JSON file with the statistics for each log file analyzed, and print the statistics to the terminal in the following format:
+
+```
+{
+    "top_ips": [
+        {
+            "192.168.1.1": 50,
+            "192.168.1.2": 30,
+            "192.168.1.3": 20
+        }
+    ],
+    "top_longest": [
+        {
+            "ip": "192.168.1.1",
+            "date": "[06/Jan/2016:12:00:02 +0100]",
+            "method": "GET",
+            "url": "/some/long/url",
+            "duration": 500
+        },
+        {
+            "ip": "192.168.1.2",
+            "date": "[06/Jan/2016:18:47:57 +0100]",
+            "method": "POST",
+            "url": "/another/long/url",
+            "duration": 400
+        },
+        {
+            "ip": "192.168.1.3",
+            "date": "[23/Dec/2015:07:27:57 +0100]",
+            "method": "GET",
+            "url": "/yet/another/long/url",
+            "duration": 300
+        }
+    ],
+    "total_stat": {
+        "GET": 70,
+        "POST": 20,
+        "HEAD": 3,
+        "PUT": 5,
+        "OPTIONS": 1,
+        "DELETE": 1
+    },
+    "total_requests": 100
+}
+``` 
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- log_parser.py
+++ log_parser.py
@@ -5,62 +5,70 @@ from collections import defaultdict
 from collections import Counter
 
 
-def main(file):
-    dict_ip = defaultdict(
-        lambda: {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0, "OPTIONS": 0}
-    )
+def main(files):
+    findex = 0
 
-    ips_count = defaultdict(int)
-    total_stat = defaultdict(int)
-    total_requests = 0
+    for file in files:
+        findex += 1
 
-    array = [-1, -1, -1]
+        requests = []
+        ips_count = defaultdict(int)
+        total_stat = defaultdict(int)
+        total_requests = 0
 
-    with open(file) as f:
-        idx = 0
-        for line in f:
-            if idx > 99:
-                break
+        with open(file) as f:
+            idx = 0
+            for line in f:
 
-            # 109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "GET /administrator/ HTTP/1.1" 200 4263
-            # "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" 7269
-            match = re.search(r"(?:(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-\s(?:[-\w]+)\s(?P<time>\[\s*("
-                              r"\d+/\D+/.*?)\])\s\"(?P<method>POST|GET|PUT|DELETE|HEAD|OPTIONS)\s("
-                              r"?P<url_path>\S+)\s+?HTTP/\d\.\d\"\s(?P<response_code>\d+?)\s(?P<duration>\d+?|-)\s\"("
-                              r"?P<url>[^\"]*))\"", line)
+                match = re.search(r"(?:(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-\s(?:[-\w\"\.@]+)\s(?P<time>\[\s*("
+                                  r"\d+/\D+/.*?)\])\s\"(?P<method>POST|GET|PUT|DELETE|HEAD|OPTIONS)\s("
+                                  r"?P<url_path>\S+)\s+?HTTP/\d\.\d\"\s(?P<response_code>\d+?)\s(?P<duration>\d+?|-)\s\"("
+                                  r"?P<url>[^\"]*))\"", line)
 
-            #ip_match = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
-            if match:
-                ip = match.group("ip")
-                ips_count[ip] += 1
-                total_requests += 1
-                #method = re.search(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line)
-                method = match.group("method")
-                dict_ip[ip][method] += 1
-                total_stat[method] += 1
-                idx += 1
-                duration = match.group("duration")
-                if duration != "-":
-                    duration = int(duration)
-                    # TBD
+                if match:
+                    ip = match.group("ip")
+                    date = match.group("time")
+                    if match.group("url") and match.group("url") != "-":
+                        url = match.group("url") + match.group("url_path")[1:]
+                    else:
+                        url = match.group("url_path")
+                    ips_count[ip] += 1
+                    total_requests += 1
+                    method = match.group("method")
+                    total_stat[method] += 1
+                    idx += 1
+                    duration = match.group("duration")
+                    if duration != "-":
+                        duration = int(duration)
+                        requests.append({
+                            "ip": ip,
+                            "date": date,
+                            "method": method,
+                            "url": url,
+                            "duration": duration
+                        })
+                else:
+                    print(line)
 
-            else:
-                print(line)
+        requests = sorted(requests, key=lambda x: -x["duration"])
+        most_common = Counter(ips_count).most_common(3)
 
-    #total_requests
-    print(total_requests)
-    #top_ips
-    print(Counter(ips_count).most_common(3))
-    #total_stat
-    print(total_stat)
-    #print(json.dumps(dict_ip, indent=4))
+        data = {"top_ips":
+            {
+                ip: count for ip, count in most_common
+            },
+            "top_longest": requests[0:3],
+            "total_stat": dict(total_stat),
+            "total_requests": total_requests
+        }
+
+        with open(f'result{findex}.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process access.log')
-    # https://docs.python.org/3/library/argparse.html
-    # https://docs.python.org/3/library/argparse.html#the-add-argument-method
-    parser.add_argument('-f', dest='file', action='store', help='Path to logfile')
+    parser.add_argument('--file', dest='files', action='store', help='Path to logfile', nargs='+')
     args = parser.parse_args()
 
-    main(args.file)
+    main(args.files)
